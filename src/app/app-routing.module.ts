0e1ab9524fd909a62403ef5ed249cc738d97b36c
Richard <richard.fagot@gmail.com>
2018-01-08 16:23:39 +0100

    Move routing in flat mode

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PhotosComponent} from './photos/photos.component';
+
+const routes: Routes = [
+  {path: 'photos', component: PhotosComponent}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
